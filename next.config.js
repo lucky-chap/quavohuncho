f823f68833060e19594b5d46b00e3a270c55f343
Quavo Huncho <wartywarthog7@gmail.com>
2022-05-17 21:54:54 +0000

    fix: :ambulance: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,15 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap'
-      }
-    ];
-  },
-  async redirects() {
-    return [
-      // portfolio archives
-      {
-        source: '/archives/v1',
-        destination: 'https://wizardly-payne-b3707b.netlify.app/',
-        permanent: true
-      }
-    ];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true
   },
   images: {
     domains: [
-      's3.us-west-2.amazonaws.com', // Images coming from Notion
-      'via.placeholder.com', // for articles that do not have a cover image
-      'images.unsplash.com', // For blog posts that use an external cover ima ge
-      'pbs.twimg.com', // Twitter Profile Picture
+      's3.us-west-2.amazonaws.com',
+      'via.placeholder.com',
+      'images.unsplash.com',
+      'pbs.twimg.com',
       'dwgyu36up6iuz.cloudfront.net',
       'cdn.hashnode.com',
       'res.craft.do',
